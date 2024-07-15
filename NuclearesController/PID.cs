e4553d94fa5419b1d09779d32abe4bd6e7d078ff
GHXX <GHXX@gmx.at>
2024-07-15 18:32:01 +0200

    Add project files.

---
+++ NuclearesController/PID.cs
@@ -0,0 +1,68 @@
+﻿namespace NuclearesController
+{
+    internal class PID
+    {
+        public double KP { get; }
+        public double KI { get; }
+        public double KD { get; }
+
+        private double integratorValue = 0;
+        private readonly (double, double) integratorMinMax;
+        private readonly TimeSpan derivativeTime;
+        private readonly double inverseEffectFactor;
+        private (int, double)? lastDatapoint = null;
+        private (int, double)? currentDifferentDDatapoint = null;
+        private (int, double)? lastDifferentDDatapoint = null;
+
+        private (double, double, double) lastVals = (0, 0, 0);
+
+        public PID(double kP, double kI, double kD, double initialOutput, bool inverseEffect, (double, double)? integratorMinMax, TimeSpan derivativeTime)
+        {
+            this.KP = kP;
+            this.KI = kI;
+            this.KD = kD;
+            this.integratorValue = initialOutput;
+            this.integratorMinMax = integratorMinMax ?? (double.MinValue, double.MaxValue);
+            this.derivativeTime = derivativeTime;
+            this.inverseEffectFactor = inverseEffect ? -1 : 1;
+        }
+
+        public double Step(int timeSecondsNow, double expected, double actual) => Step(timeSecondsNow, expected - actual);
+        public double Step(int timeSecondsNow, double error)
+        {
+            var currDatapoint = (timeSecondsNow, error);
+            double cP = error * this.KP * this.inverseEffectFactor;
+            double cI = 0;
+            double cD = 0;
+
+            if (this.currentDifferentDDatapoint == null || double.Abs(currDatapoint.Item2 - this.currentDifferentDDatapoint.Value.Item2) > 1e-8) // new changed datapoint
+            {
+                this.lastDifferentDDatapoint = this.currentDifferentDDatapoint;
+                this.currentDifferentDDatapoint = currDatapoint;
+            }
+
+            if (this.lastDifferentDDatapoint != null && this.currentDifferentDDatapoint != null)
+            {
+                var deltaT = this.currentDifferentDDatapoint.Value.Item1 - this.lastDifferentDDatapoint.Value.Item1;
+                var deltaE = this.currentDifferentDDatapoint.Value.Item2 - this.lastDifferentDDatapoint.Value.Item2;
+                cD = this.KD * (deltaE / deltaT) * this.inverseEffectFactor;
+            }
+
+            bool ciLimited = false;
+            if (this.lastDatapoint != null)
+            {
+                var deltaT = currDatapoint.Item1 - this.lastDatapoint.Value.Item1;
+                var unclamped = this.integratorValue + this.KI * error * deltaT * this.inverseEffectFactor;
+                ciLimited = unclamped > this.integratorMinMax.Item2;
+                this.integratorValue = Math.Clamp(unclamped, this.integratorMinMax.Item1, this.integratorMinMax.Item2);
+            }
+
+            cI = this.integratorValue;
+            this.lastDatapoint = currDatapoint;
+            this.lastVals = (cP, cI, cD);
+            return ciLimited ? this.integratorMinMax.Item2 : cP + cI + cD;
+        }
+
+        public double[] GetPIDComponents() => [this.lastVals.Item1, this.lastVals.Item2, this.lastVals.Item3];
+    }
+}
---
+++ NuclearesController/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace NuclearesController
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Starting controller...");
+            Console.Title = "Nucleares Controller";
+
+            const int PORT = 8181;
+            var requestTimeout = TimeSpan.FromSeconds(2);
+            var hc = new HttpClient() { BaseAddress = new($"http://localhost:{PORT}/") };
+            async Task<T> GetVariableAsync<T>(string varname) where T : IParsable<T>
+                => T.Parse(await hc.GetStringAsync($"?variable={varname}", new CancellationTokenSource(requestTimeout).Token), null);
+
+            async Task SetVariableAsync(string varname, float value) => await hc.PostAsync($"?variable={varname}&value={value}", null);
+
+            var rodStartPercentage = await GetVariableAsync<float>("RODS_POS_ACTUAL");
+            var mainPid = new PID(0.00005, 0.00001, 0.02, rodStartPercentage, true, (0, 100), TimeSpan.FromSeconds(15));
+            string padright = new string(' ', 32);
+            const float absorptionCapacity = 10000;
+            const float targetPowerOutput = (absorptionCapacity / 2) * (0.75f);
+            int currentTimestamp = 0;
+            while (true)
+            {
+                while (true)
+                {
+                    var nextTs = await GetVariableAsync<int>("TIME_STAMP"); // number representing minutes since start of game, ingame time
+                    if (nextTs != currentTimestamp) { currentTimestamp = nextTs; break; }
+                    await Task.Delay(500);
+                }
+
+                var currPowerOutput = await GetVariableAsync<float>("AUX_EFFECTIVELY_DERIVED_ENERGY_KW");
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine("Cool reactor controller :)))))");
+
+                var newRodLevel = Math.Clamp(mainPid.Step(currentTimestamp, targetPowerOutput, currPowerOutput), 0, 100);
+                Console.WriteLine($"Curr/Targeted power output: {currPowerOutput}/{targetPowerOutput} E={targetPowerOutput - currPowerOutput}" + padright);
+                Console.WriteLine($"Current PID output (new rod level): {newRodLevel}" + padright);
+                Console.WriteLine($"Raw P/I/D components: {mainPid.GetPIDComponents().Select(x => x.ToString()).Aggregate((a, b) => $"{a}/{b}")}" + padright);
+                await SetVariableAsync("RODS_POS_ORDERED", (float)newRodLevel);
+            }
+        }
+    }
+}
