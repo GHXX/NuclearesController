4f0109502dd2e2369974af6e4655b37c36f4c393
GHXX <GHXX@gmx.at>
2024-07-15 21:41:02 +0200

    dual PID setup

--- NuclearesController/PID.cs
+++ NuclearesController/PID.cs
@@ -8,7 +8,6 @@
 
         private double integratorValue = 0;
         private readonly (double, double) integratorMinMax;
-        private readonly TimeSpan derivativeTime;
         private readonly double inverseEffectFactor;
         private (int, double)? lastDatapoint = null;
         private (int, double)? currentDifferentDDatapoint = null;
@@ -16,20 +15,21 @@
 
         private (double, double, double) lastVals = (0, 0, 0);
 
-        public PID(double kP, double kI, double kD, double initialOutput, bool inverseEffect, (double, double)? integratorMinMax, TimeSpan derivativeTime)
+        public PID(double kP, double kI, double kD, double initialOutput, bool inverseEffect, (double, double)? integratorMinMax)
         {
             this.KP = kP;
             this.KI = kI;
             this.KD = kD;
             this.integratorValue = initialOutput;
             this.integratorMinMax = integratorMinMax ?? (double.MinValue, double.MaxValue);
-            this.derivativeTime = derivativeTime;
             this.inverseEffectFactor = inverseEffect ? -1 : 1;
         }
 
-        public double Step(int timeSecondsNow, double expected, double actual) => Step(timeSecondsNow, expected - actual);
-        public double Step(int timeSecondsNow, double error)
+        public string StateString { get; private set; } = "unknown";
+        public double Step(int timeSecondsNow, double target, double actual)
         {
+            double error = target - actual;
+
             var currDatapoint = (timeSecondsNow, error);
             double cP = error * this.KP * this.inverseEffectFactor;
             double cI = 0;
@@ -53,14 +53,17 @@
             {
                 var deltaT = currDatapoint.Item1 - this.lastDatapoint.Value.Item1;
                 var unclamped = this.integratorValue + this.KI * error * deltaT * this.inverseEffectFactor;
-                ciLimited = unclamped > this.integratorMinMax.Item2;
+                ciLimited = (unclamped > this.integratorMinMax.Item2) || (unclamped < this.integratorMinMax.Item1);
                 this.integratorValue = Math.Clamp(unclamped, this.integratorMinMax.Item1, this.integratorMinMax.Item2);
             }
 
             cI = this.integratorValue;
             this.lastDatapoint = currDatapoint;
             this.lastVals = (cP, cI, cD);
-            return ciLimited ? this.integratorMinMax.Item2 : cP + cI + cD;
+
+            this.StateString = $"Curr/Target {actual}/{target} E:{error}";
+
+            return ciLimited ? cI : cP + cI + cD;
         }
 
         public double[] GetPIDComponents() => [this.lastVals.Item1, this.lastVals.Item2, this.lastVals.Item3];
--- NuclearesController/Program.cs
+++ NuclearesController/Program.cs
@@ -2,42 +2,82 @@
 {
     internal class Program
     {
+
+        const int PORT = 8080;
+        static readonly TimeSpan requestTimeout = TimeSpan.FromSeconds(2);
+        static HttpClient hc = new HttpClient() { BaseAddress = new($"http://localhost:{PORT}/") };
+        static async Task<T> GetVariableAsync<T>(string varname) where T : IParsable<T>
+            => T.Parse(await hc.GetStringAsync($"?variable={varname}", new CancellationTokenSource(requestTimeout).Token), null);
+
+        static async Task SetVariableAsync(string varname, float value) => await hc.PostAsync($"?variable={varname}&value={value}", null);
+
+        static int currentTimestamp = 0;
+        static async Task WaitForNextTimeStepAsync()
+        {
+            while (true)
+            {
+                var nextTs = await GetVariableAsync<int>("TIME_STAMP"); // number representing minutes since start of game, ingame time
+                if (nextTs != currentTimestamp) { currentTimestamp = nextTs; break; }
+                await Task.Delay(500);
+            }
+        }
+
         static async Task Main(string[] args)
         {
             Console.WriteLine("Starting controller...");
             Console.Title = "Nucleares Controller";
 
-            const int PORT = 8181;
-            var requestTimeout = TimeSpan.FromSeconds(2);
-            var hc = new HttpClient() { BaseAddress = new($"http://localhost:{PORT}/") };
-            async Task<T> GetVariableAsync<T>(string varname) where T : IParsable<T>
-                => T.Parse(await hc.GetStringAsync($"?variable={varname}", new CancellationTokenSource(requestTimeout).Token), null);
-
-            async Task SetVariableAsync(string varname, float value) => await hc.PostAsync($"?variable={varname}&value={value}", null);
-
-            var rodStartPercentage = await GetVariableAsync<float>("RODS_POS_ACTUAL");
-            var mainPid = new PID(0.00005, 0.00001, 0.02, rodStartPercentage, true, (0, 100), TimeSpan.FromSeconds(15));
-            string padright = new string(' ', 32);
             const float absorptionCapacity = 10000;
             const float targetPowerOutput = (absorptionCapacity / 2) * (0.75f);
-            int currentTimestamp = 0;
+
+        retry: // retry marker for from inside catch block
+            try
+            {
+                await hc.GetStringAsync("?variable=CORE_TEMP");
+            }
+            catch { Console.WriteLine("Waiting for webserver to be online..."); goto retry; }
+            Console.Clear();
+
+
+            string padright = new string(' ', 32);
+
+            double targetCoreTemp = await GetVariableAsync<float>("CORE_TEMP");
+            double rodStartPercentage = await GetVariableAsync<float>("RODS_POS_ACTUAL");
+
+            var energyToCoreTempPid = new PID(0.00005, 0.00001, 0.02, targetCoreTemp, false, (120, 450));
+            var coreTempToRodsPid = new PID(0.01, 0.01, 0, rodStartPercentage, true, (0, 100));
             while (true)
             {
-                while (true)
-                {
-                    var nextTs = await GetVariableAsync<int>("TIME_STAMP"); // number representing minutes since start of game, ingame time
-                    if (nextTs != currentTimestamp) { currentTimestamp = nextTs; break; }
-                    await Task.Delay(500);
-                }
-
+                await WaitForNextTimeStepAsync();
+                // PID (DerivedEnergy --> CoreTemp)
                 var currPowerOutput = await GetVariableAsync<float>("AUX_EFFECTIVELY_DERIVED_ENERGY_KW");
+                targetCoreTemp = energyToCoreTempPid.Step(currentTimestamp, targetPowerOutput, currPowerOutput);
+                targetCoreTemp = 200;
+
+                // ------
+                // PID (CoreTemp --> Rods)
+                var coreTempActual = await GetVariableAsync<float>("CORE_TEMP");
+                var newRodLevel = coreTempToRodsPid.Step(currentTimestamp, targetCoreTemp, coreTempActual);
+                // ------
+
+
                 Console.SetCursorPosition(0, 0);
                 Console.WriteLine("Cool reactor controller :)))))");
 
-                var newRodLevel = Math.Clamp(mainPid.Step(currentTimestamp, targetPowerOutput, currPowerOutput), 0, 100);
                 Console.WriteLine($"Curr/Targeted power output: {currPowerOutput}/{targetPowerOutput} E={targetPowerOutput - currPowerOutput}" + padright);
+                Console.WriteLine($"Current PID output (new desired core temp): {targetCoreTemp}" + padright);
                 Console.WriteLine($"Current PID output (new rod level): {newRodLevel}" + padright);
-                Console.WriteLine($"Raw P/I/D components: {mainPid.GetPIDComponents().Select(x => x.ToString()).Aggregate((a, b) => $"{a}/{b}")}" + padright);
+
+                int pidx = 0;
+                foreach (var pid in new[] { energyToCoreTempPid, coreTempToRodsPid })
+                {
+                    void pfixLog(string m) => Console.WriteLine($"[PID {pidx}] " + m);
+                    pfixLog(pid.StateString);
+                    pfixLog($"Raw P/I/D components: {string.Join("/", pid.GetPIDComponents().Select(x => x.ToString()))}" + padright);
+
+                    pidx++;
+                }
+
                 await SetVariableAsync("RODS_POS_ORDERED", (float)newRodLevel);
             }
         }
