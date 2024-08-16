292f221377ba5131aa9a0af49f04f5d0cdc8f5b7
GHXX <GHXX@gmx.at>
2024-08-16 10:41:12 +0200

    improvements

--- NuclearesController/PID.cs
+++ NuclearesController/PID.cs
@@ -1,40 +1,42 @@
-﻿namespace NuclearesController
+﻿namespace NuclearesController;
+
+internal class PID
 {
-    internal class PID
+    public double KP { get; }
+    public double KI { get; }
+    public double KD { get; }
+
+    private double integratorValue = 0;
+    private readonly (double, double) integratorMinMax;
+    private readonly double inverseEffectFactor;
+    private (int, double)? lastDatapoint = null;
+    private (int, double)? currentDifferentDDatapoint = null;
+    private (int, double)? lastDifferentDDatapoint = null;
+
+    private (double, double, double) lastVals = (0, 0, 0);
+
+    public PID(double kP, double kI, double kD, double initialOutput, bool inverseEffect, (double, double)? integratorMinMax)
     {
-        public double KP { get; }
-        public double KI { get; }
-        public double KD { get; }
+        this.KP = kP;
+        this.KI = kI;
+        this.KD = kD;
+        this.integratorValue = initialOutput;
+        this.integratorMinMax = integratorMinMax ?? (double.MinValue, double.MaxValue);
+        this.inverseEffectFactor = inverseEffect ? -1 : 1;
+    }
 
-        private double integratorValue = 0;
-        private readonly (double, double) integratorMinMax;
-        private readonly double inverseEffectFactor;
-        private (int, double)? lastDatapoint = null;
-        private (int, double)? currentDifferentDDatapoint = null;
-        private (int, double)? lastDifferentDDatapoint = null;
+    public string StateString { get; private set; } = "unknown";
+    public double Step(int timeSecondsNow, double target, double actual, double? directDelta = null)
+    {
+        double error = target - actual;
 
-        private (double, double, double) lastVals = (0, 0, 0);
-
-        public PID(double kP, double kI, double kD, double initialOutput, bool inverseEffect, (double, double)? integratorMinMax)
+        var currDatapoint = (timeSecondsNow, error);
+        double cP = error * this.KP * this.inverseEffectFactor;
+        double cI = 0;
+        double cD = 0;
+        double deriv = 0;
+        if (directDelta == null)
         {
-            this.KP = kP;
-            this.KI = kI;
-            this.KD = kD;
-            this.integratorValue = initialOutput;
-            this.integratorMinMax = integratorMinMax ?? (double.MinValue, double.MaxValue);
-            this.inverseEffectFactor = inverseEffect ? -1 : 1;
-        }
-
-        public string StateString { get; private set; } = "unknown";
-        public double Step(int timeSecondsNow, double target, double actual)
-        {
-            double error = target - actual;
-
-            var currDatapoint = (timeSecondsNow, error);
-            double cP = error * this.KP * this.inverseEffectFactor;
-            double cI = 0;
-            double cD = 0;
-
             if (this.currentDifferentDDatapoint == null || double.Abs(currDatapoint.Item2 - this.currentDifferentDDatapoint.Value.Item2) > 1e-8) // new changed datapoint
             {
                 this.lastDifferentDDatapoint = this.currentDifferentDDatapoint;
@@ -45,27 +47,30 @@
             {
                 var deltaT = this.currentDifferentDDatapoint.Value.Item1 - this.lastDifferentDDatapoint.Value.Item1;
                 var deltaE = this.currentDifferentDDatapoint.Value.Item2 - this.lastDifferentDDatapoint.Value.Item2;
-                cD = this.KD * (deltaE / deltaT) * this.inverseEffectFactor;
+                deriv = (deltaE / deltaT);
             }
+        }
+        else
+            deriv = directDelta.Value;
 
-            bool ciLimited = false;
-            if (this.lastDatapoint != null)
-            {
-                var deltaT = currDatapoint.Item1 - this.lastDatapoint.Value.Item1;
-                var unclamped = this.integratorValue + this.KI * error * deltaT * this.inverseEffectFactor;
-                ciLimited = (unclamped > this.integratorMinMax.Item2) || (unclamped < this.integratorMinMax.Item1);
-                this.integratorValue = Math.Clamp(unclamped, this.integratorMinMax.Item1, this.integratorMinMax.Item2);
-            }
-
-            cI = this.integratorValue;
-            this.lastDatapoint = currDatapoint;
-            this.lastVals = (cP, cI, cD);
-
-            this.StateString = $"Curr/Target {actual}/{target} E:{error}";
-
-            return ciLimited ? cI : cP + cI + cD;
+        cD = this.KD * deriv * this.inverseEffectFactor;
+        bool ciLimited = false;
+        if (this.lastDatapoint != null)
+        {
+            var deltaT = currDatapoint.Item1 - this.lastDatapoint.Value.Item1;
+            var unclamped = this.integratorValue + this.KI * error * deltaT * this.inverseEffectFactor;
+            ciLimited = (unclamped > this.integratorMinMax.Item2) || (unclamped < this.integratorMinMax.Item1);
+            this.integratorValue = Math.Clamp(unclamped, this.integratorMinMax.Item1, this.integratorMinMax.Item2);
         }
 
-        public double[] GetPIDComponents() => [this.lastVals.Item1, this.lastVals.Item2, this.lastVals.Item3];
+        cI = this.integratorValue;
+        this.lastDatapoint = currDatapoint;
+        this.lastVals = (cP, cI, cD);
+
+        this.StateString = $"Curr/Target {actual}/{target} E:{error}";
+
+        return ciLimited ? cI : cP + cI + cD;
     }
+
+    public double[] GetPIDComponents() => [this.lastVals.Item1, this.lastVals.Item2, this.lastVals.Item3];
 }
--- NuclearesController/Program.cs
+++ NuclearesController/Program.cs
@@ -1,28 +1,39 @@
-﻿namespace NuclearesController
+﻿namespace NuclearesController;
+
+internal class Program
 {
-    internal class Program
+    private const int PORT = 8785;
+    private static readonly TimeSpan requestTimeout = TimeSpan.FromSeconds(2);
+    private static HttpClient hc = new HttpClient() { BaseAddress = new($"http://localhost:{PORT}/") };
+
+    public static async Task<string> GetVariableRawAsync(string varname) => await hc.GetStringAsync($"?variable={varname}", new CancellationTokenSource(requestTimeout).Token);
+    public static async Task<T> GetVariableAsync<T>(string varname) where T : IParsable<T> => T.Parse(await GetVariableRawAsync(varname), null);
+
+    public static async Task SetVariableAsync(string varname, object value) => await hc.PostAsync($"?variable={varname}&value={value}", null);
+
+    private static int currentTimestamp = 0;
+
+    private static async Task WaitForNextTimeStepAsync()
     {
-
-        const int PORT = 8080;
-        static readonly TimeSpan requestTimeout = TimeSpan.FromSeconds(2);
-        static HttpClient hc = new HttpClient() { BaseAddress = new($"http://localhost:{PORT}/") };
-        static async Task<T> GetVariableAsync<T>(string varname) where T : IParsable<T>
-            => T.Parse(await hc.GetStringAsync($"?variable={varname}", new CancellationTokenSource(requestTimeout).Token), null);
-
-        static async Task SetVariableAsync(string varname, float value) => await hc.PostAsync($"?variable={varname}&value={value}", null);
-
-        static int currentTimestamp = 0;
-        static async Task WaitForNextTimeStepAsync()
+        while (true)
         {
-            while (true)
-            {
-                var nextTs = await GetVariableAsync<int>("TIME_STAMP"); // number representing minutes since start of game, ingame time
-                if (nextTs != currentTimestamp) { currentTimestamp = nextTs; break; }
-                await Task.Delay(500);
-            }
+            var nextTs = await GetVariableAsync<int>("TIME_STAMP"); // number representing minutes since start of game, ingame time
+            if (nextTs != currentTimestamp) { currentTimestamp = nextTs; break; }
+            await Task.Delay(500);
         }
+    }
 
-        static async Task Main(string[] args)
+    private static async Task WaitForWebserverAvailableAsync()
+    {
+    retry: // retry marker for from inside catch block
+        try { await hc.GetStringAsync("?variable=CORE_TEMP"); }
+        catch { Console.WriteLine("Waiting for webserver to be online..."); goto retry; }
+    }
+
+    private static async Task Main(string[] args)
+    {
+    restart:
+        try
         {
             Console.WriteLine("Starting controller...");
             Console.Title = "Nucleares Controller";
@@ -30,56 +41,75 @@
             const float absorptionCapacity = 10000;
             const float targetPowerOutput = (absorptionCapacity / 2) * (0.75f);
 
-        retry: // retry marker for from inside catch block
-            try
-            {
-                await hc.GetStringAsync("?variable=CORE_TEMP");
-            }
-            catch { Console.WriteLine("Waiting for webserver to be online..."); goto retry; }
+            await WaitForWebserverAvailableAsync();
             Console.Clear();
 
+
+
+            var variablesToSet = new Dictionary<string, string>();
+            void SetVariable(string name, object value) => variablesToSet[name] = value.ToString()!;
 
             string padright = new string(' ', 32);
 
             double targetCoreTemp = await GetVariableAsync<float>("CORE_TEMP");
+            const float desiredCoreTemp = 360f;
             double rodStartPercentage = await GetVariableAsync<float>("RODS_POS_ACTUAL");
-
-            var energyToCoreTempPid = new PID(0.00005, 0.00001, 0.02, targetCoreTemp, false, (120, 450));
             var coreTempToRodsPid = new PID(0.01, 0.01, 0, rodStartPercentage, true, (0, 100));
+
+            const float targetSecondaryLevel = 2800f;
+            var secondaryLevelPids = Enumerable.Range(0, 3).Select(async i => new PID(0.0005, 0.01, 0, await GetVariableAsync<float>($"COOLANT_SEC_CIRCULATION_PUMP_{i}_ORDERED_SPEED"), false, (0, 100))).Select(x => x.Result).ToArray();
+
+            const float desiredCondenserTemp = 65f;
+            var condenserPumpSpeedPid = new PID(0.00005, 0.05, 0.01, await GetVariableAsync<float>("CONDENSER_CIRCULATION_PUMP_ORDERED_SPEED"), true, (0, 100));
+
+            const float desiredCondenserLevelMin = 27_000f;
+            const float desiredCondenserLevelMax = 30_000f;
+
+            //var energyToCoreTempPid = new PID(0.00005, 0.0001, 0.02, targetCoreTemp, false, (170, 450));
+            //var coreTempToRodsPid = new PID(0.01, 0.01, 0, rodStartPercentage, true, (0, 100));
             while (true)
             {
                 await WaitForNextTimeStepAsync();
-                // PID (DerivedEnergy --> CoreTemp)
-                var currPowerOutput = await GetVariableAsync<float>("AUX_EFFECTIVELY_DERIVED_ENERGY_KW");
-                targetCoreTemp = energyToCoreTempPid.Step(currentTimestamp, targetPowerOutput, currPowerOutput);
-                targetCoreTemp = 200;
+                variablesToSet.Clear();
+                var coreTempCurrent = await GetVariableAsync<float>("CORE_TEMP");
+                var reactivityzerobased = await GetVariableAsync<float>("CORE_STATE_CRITICALITY");
 
-                // ------
-                // PID (CoreTemp --> Rods)
-                var coreTempActual = await GetVariableAsync<float>("CORE_TEMP");
-                var newRodLevel = coreTempToRodsPid.Step(currentTimestamp, targetCoreTemp, coreTempActual);
-                // ------
+                SetVariable("RODS_POS_ORDERED", coreTempToRodsPid.Step(currentTimestamp, desiredCoreTemp, coreTempCurrent, reactivityzerobased));
+                for (int i = 0; i < 3; i++)
+                {
+                    var currSecCoolant = await GetVariableAsync<float>($"COOLANT_SEC_{i}_VOLUME");
+                    SetVariable($"COOLANT_SEC_CIRCULATION_PUMP_{i}_ORDERED_SPEED", secondaryLevelPids[i].Step(currentTimestamp, targetSecondaryLevel, currSecCoolant).ToString("N2"));
+                }
+
+                var condenserTempCurrent = await GetVariableAsync<float>("CONDENSER_TEMPERATURE");
+                SetVariable("CONDENSER_CIRCULATION_PUMP_ORDERED_SPEED", condenserPumpSpeedPid.Step(currentTimestamp, desiredCondenserTemp, condenserTempCurrent));
+
+                var condenserLevelCurrent = await GetVariableAsync<float>("CONDENSER_VOLUME");
+                if (condenserLevelCurrent < desiredCondenserLevelMin)
+                    SetVariable("FREIGHT_PUMP_CONDENSER_ACTIVE", true);
+                else if (condenserLevelCurrent > desiredCondenserLevelMax)
+                    SetVariable("FREIGHT_PUMP_CONDENSER_ACTIVE", false);
+
+                foreach (var (k, v) in variablesToSet)
+                {
+                    await SetVariableAsync(k, v);
+                }
 
 
                 Console.SetCursorPosition(0, 0);
                 Console.WriteLine("Cool reactor controller :)))))");
-
-                Console.WriteLine($"Curr/Targeted power output: {currPowerOutput}/{targetPowerOutput} E={targetPowerOutput - currPowerOutput}" + padright);
-                Console.WriteLine($"Current PID output (new desired core temp): {targetCoreTemp}" + padright);
-                Console.WriteLine($"Current PID output (new rod level): {newRodLevel}" + padright);
-
-                int pidx = 0;
-                foreach (var pid in new[] { energyToCoreTempPid, coreTempToRodsPid })
-                {
-                    void pfixLog(string m) => Console.WriteLine($"[PID {pidx}] " + m);
-                    pfixLog(pid.StateString);
-                    pfixLog($"Raw P/I/D components: {string.Join("/", pid.GetPIDComponents().Select(x => x.ToString()))}" + padright);
-
-                    pidx++;
-                }
-
-                await SetVariableAsync("RODS_POS_ORDERED", (float)newRodLevel);
+                Console.WriteLine($"New rod level: {variablesToSet["RODS_POS_ORDERED"]}" + padright);
+                Console.WriteLine($"Ordered secondary pumpspeeds A/B/C: {string.Join('/', Enumerable.Range(0, 3).Select(i => variablesToSet[$"COOLANT_SEC_CIRCULATION_PUMP_{i}_ORDERED_SPEED"]))}");
+                Console.WriteLine($"Ordered condenser speed: {variablesToSet["CONDENSER_CIRCULATION_PUMP_ORDERED_SPEED"]}" + padright);
+                Console.WriteLine(padright + padright + padright);
+                Console.SetCursorPosition(0, 0);
             }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            await Task.Delay(10_000);
+            goto restart;
         }
     }
 }
